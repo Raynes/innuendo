1234fd1d8dd5d70eddb21e2d46f8fc281c16b0e7
Anthony Grimes <i@raynes.me>
2012-05-04 22:23:46 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject innuendo "0.1.2"
   :description "A simple API library for refheap.com"
-  :dependencies [[clojure "1.3.0"]
-                 [clj-http "0.2.6"]
-                 [cheshire "2.0.4"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.4.0"]
+                 [cheshire "4.0.0"]])
