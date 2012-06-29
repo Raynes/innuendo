bef7322520790ab1f6e881356190d7ce6348ff79
Anthony Grimes <i@raynes.me>
2012-06-29 03:12:23 -0500

    Get this release business straightened out.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject innuendo "0.1.3"
+(defproject innuendo "0.1.5"
   :description "A simple API library for refheap.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.0"]])
