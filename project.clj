c4ed54476350fe68c12d678c29e105247901a736
Anthony Grimes <i@raynes.me>
2012-06-29 03:13:14 -0500

    Add license info.

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject innuendo "0.1.5"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :url "https://github.com/Raynes/innuendo"
   :description "A simple API library for refheap.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.4.0"]])
